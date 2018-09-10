0987e2c8514666363ff08368e2bb2e3323d90e9f
Andy Tang <and13.tang@gmail.com>
2018-09-11 00:12:24 +0700

    Project setup

---
+++ task-1/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+// data set to be filtered
+const data = [
+  { name: "Football" },
+  { name: "Baseball" },
+  { name: "Basketball" },
+  { name: "iPod Touch" },
+  { name: "iPhone 5" },
+  { name: "Nexus 7" }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
